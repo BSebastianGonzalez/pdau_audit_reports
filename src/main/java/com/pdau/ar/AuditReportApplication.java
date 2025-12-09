b8a13f3cd2af8b08866d0f7bd72deb7d7a55ed4f
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-12-09 00:25:08 -0500

    first commit

---
+++ src/main/java/com/pdau/ar/AuditReportApplication.java
@@ -0,0 +1,13 @@
+package com.pdau.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdau/ar/AuditReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdau.ar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
