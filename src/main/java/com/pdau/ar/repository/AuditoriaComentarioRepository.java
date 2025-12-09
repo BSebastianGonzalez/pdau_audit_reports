36c23f63bdbdbb4b3c793631bc4d6a509503be2e
lesg1151860 <luiseduardosg@ufps.edu.co>
2025-12-09 18:16:12 -0500

    chore: added Comentario and Archivamiento queue connection

---
+++ src/main/java/com/pdau/ar/repository/AuditoriaComentarioRepository.java
@@ -0,0 +1,7 @@
+package com.pdau.ar.repository;
+
+import com.pdau.ar.model.AuditoriaComentario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditoriaComentarioRepository extends JpaRepository<AuditoriaComentario, Long> {
+}
